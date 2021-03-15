d7c689efe4a0ae91923f48bf0212cd4b0fd53e88
Afreed15 <Afreed.vs786@gmail.com>
2021-03-15 16:40:48 +0530

    Commit

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace WebAppexegit.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<WebAppexegit.Data.WebAppexegitContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(WebAppexegit.Data.WebAppexegitContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
