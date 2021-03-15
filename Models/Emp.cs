1866d67fe37d3fd641376e83df65a94ffbf412dd
Afreed15 <Afreed.vs786@gmail.com>
2021-03-15 15:41:43 +0530

    second Commit

--- Models/Emp.cs
+++ Models/Emp.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace WebAppexegit.Models
 {
+    [Table("Emp")]
     public class Emp
     {
+        [Key]
+        public  int Empid { get; set; }
+        public string EmpName { get; set; }
+        public string  Designation { get; set; }
+        public DateTime? EDoj { get; set; }
     }
 }
